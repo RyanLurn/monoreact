4464761cb1795da2c5fe46ceac2611521ab2c774
Ryan Luong <lumintriet@gmail.com>
2025-11-26 22:14:45 +0700

    chore(workspace): add tailwind class sorting plugin for Prettier

--- apps/next-app/src/app/page.tsx
+++ apps/next-app/src/app/page.tsx
@@ -2,7 +2,7 @@ import { Button } from "@repo/ui/components/button";
 
 export default function HomePage() {
   return (
-    <main className="h-dvh flex items-center justify-center">
+    <main className="flex h-dvh items-center justify-center">
       <Button>Click me</Button>
     </main>
   );
