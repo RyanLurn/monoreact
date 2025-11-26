2c3f2c549da517dd186e600208a1b585d5b04713
Ryan Luong <lumintriet@gmail.com>
2025-11-26 22:19:10 +0700

    feat(next-app/ui): use globals.css stylesheet

--- apps/next-app/src/app/page.tsx
+++ apps/next-app/src/app/page.tsx
@@ -2,7 +2,7 @@ import { Button } from "@repo/ui/components/button";
 
 export default function HomePage() {
   return (
-    <main className="flex h-dvh items-center justify-center">
+    <main className="flex h-dvh animate-in items-center justify-center fade-in zoom-in">
       <Button>Click me</Button>
     </main>
   );
