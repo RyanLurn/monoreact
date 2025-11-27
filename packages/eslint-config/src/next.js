ea0df14dceb71b06b431a408122a9b5aeeefd72a
Ryan Luong <lumintriet@gmail.com>
2025-11-27 08:41:35 +0700

    refactor(eslint-config): use extend instead of spread

--- packages/eslint-config/src/next.js
+++ packages/eslint-config/src/next.js
@@ -1,13 +1,19 @@
-import prettier from "eslint-config-prettier/flat";
 import { defineConfig } from "eslint/config";
 import next from "@next/eslint-plugin-next";
 import { reactConfig } from "#react.js";
+import globals from "globals";
 
 const nextConfig = defineConfig([
-  ...reactConfig,
+  {
+    languageOptions: {
+      globals: {
+        ...globals.node,
+      },
+    },
+    extends: [reactConfig],
+  },
   next.configs.recommended,
   next.configs["core-web-vitals"],
-  prettier,
 ]);
 
 export { nextConfig };
--- packages/eslint-config/src/react.js
+++ packages/eslint-config/src/react.js
@@ -1,14 +1,20 @@
-import prettier from "eslint-config-prettier/flat";
 import reactHooks from "eslint-plugin-react-hooks";
 import { defineConfig } from "eslint/config";
 import jsxA11y from "eslint-plugin-jsx-a11y";
 import { baseConfig } from "#base.js";
+import globals from "globals";
 
 const reactConfig = defineConfig([
-  ...baseConfig,
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+      },
+    },
+    extends: [baseConfig],
+  },
   reactHooks.configs.flat.recommended,
   jsxA11y.flatConfigs.recommended,
-  prettier,
 ]);
 
 export { reactConfig };
